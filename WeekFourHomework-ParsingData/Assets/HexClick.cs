896e2e946d8b1f4f2094ddebde7d52bac4f24f74
Alessia <alessia@mipsscan.com>
2021-02-27 21:47:18 -0500

    Initial Push

---
+++ WeekFourHomework-ParsingData/Assets/HexClick.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexClick : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log(this.gameObject.name);
+    }
+}
